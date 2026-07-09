e2ab360a6e7161bd25447a16db250006cbfb7d19
Ashwini-2021 <80737225+Ashwini-2021@users.noreply.github.com>
2026-07-09 13:59:32 +0530

    Add: is-subsequence - submission-5

---
+++ Data Structures & Algorithms/is-subsequence/submission-5.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public bool IsSubsequence(string s, string t) {
+        if(t=="")
+            return false;
+
+        int j=0;
+        int sLength =s.Length,tLength=t.Length;
+        for(int i=0;i<tLength && j<sLength;i++)
+        {
+            if(t[i]==s[j])
+            {
+                j++;
+                if(j==sLength)
+                {
+                    return true;
+                }
+            }
+        }
+
+        if(j==sLength)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
